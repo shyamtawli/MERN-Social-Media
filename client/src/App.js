70a5a432e7201e370f71f0d2157d9c4b514455a0
shyamtawli <shyamtawli2@gmail.com>
2023-02-17 20:34:50 +0530

    started frontend

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { Navigate, Routes, Route, BrowserRouter } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
+ 
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <LoginPage />} />
+          <Route path='/home' element={ <HomePage />} />
+          <Route path='/profile/:userId' element={ <ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return <div>
+        Homepage
+    </div>
+};
+
+export default HomePage;
