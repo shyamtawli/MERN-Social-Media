b324eb39702c3ac7b9c26c34483dcacf3de0d1f1
shyamtawli <shyamtawli2@gmail.com>
2023-02-27 18:17:13 +0530

    navbar completed

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,10 @@
+import { Box } from "@mui/material";
+import Navbar from "scenes/navbar";
+
 const HomePage = () => {
-    return <div>
-        Homepage
-    </div>
+    return <Box>
+        <Navbar />
+    </Box>
 };
 
 export default HomePage;
