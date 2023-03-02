6b1684a05d6e41a53dce7bb2448d816eff745914
shyamtawli <shyamtawli2@gmail.com>
2023-03-02 16:47:13 +0530

    login and register

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,41 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 const LoginPage = () => {
-    return <div>
-        <h1>Login Page</h1>
-    </div>
+  const theme = useTheme();
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        padding="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          SocialMedia
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreen ? "50%" : "93%"}
+        padding="2rem"
+        margin="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography
+          fontWeight="500"
+          variant="h5"
+          sx={{ marginBottom: "1.5rem" }}
+        >
+          Welcome to SocialMedia, Login to Continue !
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
-import postRoutes from "./routes/posts.js"
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import { createPost } from "./controllers/posts.js"
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js" 
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -25,21 +25,21 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //FILE STORAGE
 const storage = multer.diskStorage({
-    destination: function(req, file, cd) {
-        cd(null, "public/assets");
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cd) {
+    cd(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 const upload = multer({ storage });
 
@@ -47,21 +47,23 @@ const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-// ROUTES 
+// ROUTES
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
-const PORT = process.env.PORT  || 6001;
-mongoose.connect(process.env.MONGO_URL, {
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     // ADD DATA ONE TIME
     // User.insertMany(users);
     // Post.insertMany(posts);
-}).catch((error) => console.log(`${error} did not connect`));
-
+  })
+  .catch((error) => console.log(`${error} did not connect`));
